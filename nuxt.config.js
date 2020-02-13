215b709164d349e7dc0e55ae2c78e32582f30ac4
Changehee Choi <changhee.dev@gmail.com>
2020-02-13 16:05:46 +0900

    Add open graph properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,23 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: "chosung-bible",
+    title: "초성바이블 - 초성과 숫자로 간편하게 성경검색",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: "초성을 이용한 성경 검색 개발 프로젝트"
-      }
+        content: "초성과 숫자로 간편하게 성경을 검색해보세요."
+      },
+      { name: "og:type", content: "website" },
+      { name: "og:title", content: "초성바이블" },
+      {
+        name: "og:description",
+        content: "초성과 숫자로 간편하게 성경을 검색해보세요."
+      },
+      { name: "og:image", content: "/ogImage.png" },
+      { name: "og:url", content: "http://chosungbible.com" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
