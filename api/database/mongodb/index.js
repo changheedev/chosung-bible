b8060116473c7926fcf57d9294b9491c4c4f1b82
Changehee Choi <changhee.dev@gmail.com>
2020-02-14 20:52:28 +0900

    Add Log model in mongodb

--- api/database/mongodb/index.js
+++ api/database/mongodb/index.js
@@ -11,18 +11,45 @@ class MongoDB {
       console.log("=======================================================");
     });
 
+    //connection
     this._mongoose = mongoose;
     this._mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+
+    //create Log model
+    this._models = {
+      Log: this._mongoose.model("Log", {
+        useragent: {
+          isMobile: Boolean,
+          isTablet: Boolean,
+          isDesktop: Boolean,
+          isBot: Boolean,
+          browser: String,
+          version: String,
+          os: String,
+          platform: String
+        },
+        query: String,
+        state: Boolean,
+        date: Date
+      })
+    };
   }
 
   get mongoose() {
     return this._mongoose;
   }
+
+  get models() {
+    return this._models;
+  }
 }
 
-const mongoDB = new MongoDB();
+const database = new MongoDB();
 
-export const connection = mongoDB.mongoose;
+export const mongoDB = {
+  connection: database.mongoose,
+  models: database.models
+};
