be93ba0925e3bba14723f0f1fcd52b15ded349c7
Changehee Choi <changhee.dev@gmail.com>
2020-02-15 20:25:16 +0900

    Refactor nuxtServerInit()

--- store/index.js
+++ store/index.js
@@ -48,50 +48,51 @@ export const getters = {
   }
 };
 
+const createChosungMap = books => {
+  let chosungMap = new Map();
+
+  books.forEach(book => {
+    /**
+     * true => 각 글자별로 배열을 생성해주는 옵션
+     *input: '창세기'
+     *return: [['ㅊ','ㅏ','ㅇ'],['ㅅ','ㅔ'],['ㄱ','ㅣ']]
+     */
+    const disassembled = Hangul.disassemble(book.name, true);
+    let chosung = "";
+    let chosungAlpha = "";
+
+    disassembled.forEach(word => {
+      //추출한 배열의 각 첫 글자를 합친다 => 'ㅊㅅㄱ'
+      chosung += word[0];
+      /**
+       * 영어로 입력되는 경우를 위한 처리 'ㅊㅅㄱ' => 'ctr'
+       * 숫자가 아닌 경우에만 알파벳으로 변환한다
+       */
+      chosungAlpha += isNaN(word[0]) ? alphabet.get(word[0]) : word[0];
+    });
+    //만들어진 초성을 key 로 하여 map 을 만든다
+    chosungMap.set(chosung, { id: book.id, name: book.name });
+    chosungMap.set(chosungAlpha, { id: book.id, name: book.name });
+  });
+
+  return chosungMap;
+};
+
 export const actions = {
   async nuxtServerInit({ commit }, { app }) {
-    try {
-      const books = await app.$axios.get("/api/bible/books");
-      commit("setBooks", books);
-
-      let chosungMap = new Map();
-
-      books.forEach(book => {
-        /**
-         * true => 각 글자별로 배열을 생성해주는 옵션
-         *input: '창세기'
-         *return: [['ㅊ','ㅏ','ㅇ'],['ㅅ','ㅔ'],['ㄱ','ㅣ']]
-         */
-        const disassembled = Hangul.disassemble(book.name, true);
-        let chosung = "";
-        let chosungAlpha = "";
-
-        disassembled.forEach(word => {
-          //추출한 배열의 각 첫 글자를 합친다 => 'ㅊㅅㄱ'
-          chosung += word[0];
-          /**
-           * 영어로 입력되는 경우를 위한 처리 'ㅊㅅㄱ' => 'ctr'
-           * 숫자가 아닌 경우에만 알파벳으로 변환한다
-           */
-          chosungAlpha += isNaN(word[0]) ? alphabet.get(word[0]) : word[0];
-        });
-        //만들어진 초성을 key 로 하여 map 을 만든다
-        chosungMap.set(chosung, { id: book.id, name: book.name });
-        chosungMap.set(chosungAlpha, { id: book.id, name: book.name });
-      });
-
-      commit("setChosung", chosungMap);
-      console.log("Create chosung list has been established successfully.");
-    } catch (err) {
-      console.error("Create chosung list failed", err);
-    }
-
     try {
       const bibleMeta = await app.$axios.get("/api/bible/metadata");
       commit("setMetadata", bibleMeta);
       console.log("Load metadata has been established successfully.");
+
+      const books = await app.$axios.get("/api/bible/books");
+      commit("setBooks", books);
+
+      const chosungMap = createChosungMap(books);
+      commit("setChosung", chosungMap);
+      console.log("Create chosung list has been established successfully.");
     } catch (err) {
-      console.error("Load bible metadata failed", err);
+      console.error("Store init failed\n", err);
     }
   }
 };
