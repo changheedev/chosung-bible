77f13a452d9f1fbbda531c90c44cef19e9c0b04f
Changehee Choi <changhee.dev@gmail.com>
2020-02-15 21:26:13 +0900

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <nuxt />
+    <footer class="footer">
+      Copyright 2020. Changhee All rights reserved.
+    </footer>
   </div>
 </template>
 
-<style></style>
+<style>
+.footer {
+  padding: 20px;
+  text-align: center;
+  font-size: 0.7rem;
+  color: #555;
+}
+</style>
