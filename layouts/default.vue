51077ad91398fcf8d8f4a867f2e2865979f05fa8
Changehee Choi <changhee.dev@gmail.com>
2020-02-18 16:31:05 +0900

    Move review modal to footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
     <nuxt />
-    <footer class="footer">
-      Copyright 2020. Changhee All rights reserved.
-    </footer>
+    <comp-footer></comp-footer>
   </div>
 </template>
-
-<style>
-.footer {
-  padding: 20px;
-  text-align: center;
-  font-size: 0.7rem;
-  color: #555;
-}
-</style>
+<script>
+import CompFooter from "~/components/CompFooter";
+export default {
+  components: { CompFooter }
+};
+</script>
