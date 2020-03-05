ca37c974861d07e06cd7b077103180ce13ca22d5
Changehee Choi <changhee.dev@gmail.com>
2020-03-05 20:20:02 +0900

    Remove comment set body parser code

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,7 @@
-import express = require("express");
-import useragent = require("express-useragent");
-import router from "./router";
+import express = require('express');
+import bodyParser = require('body-parser');
+import useragent = require('express-useragent');
+import router from './router';
 
 class Server {
   private _app!: express.Application;
@@ -9,14 +10,14 @@ class Server {
       this._app = express();
       this.middleware();
     } catch (err) {
-      console.error("Server init failed.", err);
+      console.error('Server init failed.', err);
     }
   }
 
   middleware() {
     // //bodyParser 사용설정
-    // this.app.use(bodyParser.json()); //json 포맷의 데이터를 사용
-    // this.app.use(bodyParser.urlencoded({ extended: true })); //인코딩 된 데이터 사용
+    this.app.use(bodyParser.json()); //json 포맷의 데이터를 사용
+    this.app.use(bodyParser.urlencoded({ extended: true })); //인코딩 된 데이터 사용
 
     //useragent parser
     this._app.use(useragent.express());
