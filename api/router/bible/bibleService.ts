ef80125f6ef8a28af8826501732f0e40275fbc49
Changehee Choi <changhee.dev@gmail.com>
2020-03-05 18:35:36 +0900

    Refactor bible api with typescript

---
+++ api/router/bible/bibleService.ts
@@ -0,0 +1,134 @@
+import { Sequelize, Op } from 'sequelize';
+import database from '../../database/sequelize';
+import Bible from '../../database/sequelize/models/bible';
+import Book from '../../database/sequelize/models/book';
+
+interface BibleMetadata {
+  book: number;
+  chapters: number;
+  verses: Array<number>;
+}
+
+interface ChapterMetadata {
+  book: number;
+  chapters: number;
+}
+
+interface VerseMetadata {
+  book: number;
+  chapter: number;
+  verses: number;
+}
+
+class BibleService {
+  private static instance: BibleService;
+
+  constructor() {
+    if (!BibleService.instance) {
+      BibleService.instance = this;
+    }
+    return BibleService.instance;
+  }
+
+  private intergrateMetadata(chapterMeta: ChapterMetadata[], verseMeta: VerseMetadata[]): BibleMetadata[] {
+    let bibleMeta: BibleMetadata[] = [];
+
+    for (let i = 1; i <= 66; i++) {
+      let count = 0;
+      const versesOfCurrent: number[] = verseMeta.reduce((prev: number[], curVerseMeta: VerseMetadata) => {
+        if (curVerseMeta.book === i) {
+          prev.push(curVerseMeta.verses);
+          count++;
+        }
+        return prev;
+      }, []);
+
+      const curBibleMeta = {
+        book: chapterMeta[i - 1].book,
+        chapters: chapterMeta[i - 1].chapters,
+        verses: versesOfCurrent
+      };
+
+      bibleMeta.push(curBibleMeta);
+      verseMeta.splice(0, count);
+    }
+
+    return bibleMeta;
+  }
+
+  async getBibleMeta() {
+    try {
+      const [bookList, resultChapter, resultVerse] = await Promise.all([
+        Book.findAll(),
+        database.query('select book, max(chapter) as chapters from tbl_bible group by book'),
+        database.query('select book, chapter, count(verse) as verses from tbl_bible group by book, chapter')
+      ]);
+
+      const chapterMeta: ChapterMetadata[] = resultChapter[0] as ChapterMetadata[];
+      const verseMeta: VerseMetadata[] = resultVerse[0] as VerseMetadata[];
+
+      const bibleMeta = this.intergrateMetadata(chapterMeta, verseMeta);
+      return { books: bookList, metadata: bibleMeta };
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  private paramIsNumber(params: number[]) {
+    params.forEach(param => {
+      if (Number.isNaN(param)) return false;
+    });
+    return true;
+  }
+
+  async searchBibleByMeta(book: number, chapter: number, verse: number, page: number) {
+    if (!this.paramIsNumber([book, chapter, verse, page])) throw new Error('Params must be number');
+
+    //사용자가 입력한 성경부터 10개의 데이터를 가져온다
+    //사용자가 입력한 파라미터를 서브쿼리로 이용
+    try {
+      const result = await Bible.findAll({
+        where: {
+          id: {
+            [Op.gte]: Sequelize.literal(
+              `(select id from tbl_bible where book = ${book} and chapter = ${chapter} and verse = ${verse})`
+            )
+          }
+        },
+        offset: page * 10,
+        limit: 10
+      });
+      return result;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  async searchBibleByKeyword(keyword: string, page: number) {
+    if (!this.paramIsNumber([page])) throw new Error('Page is must be number');
+
+    try {
+      const keywordTokens = keyword.split(' ');
+      const result = await Bible.findAll({
+        where: Sequelize.literal(`MATCH (content) AGAINST ('${keywordTokens.join(' ')}' in boolean mode)`),
+        order: [
+          Sequelize.literal(`MATCH (content) AGAINST ('"${keyword}"' in boolean mode) DESC`),
+          Sequelize.literal(
+            `MATCH (content) AGAINST ('${keywordTokens.map(token => `+${token}`).join(' ')}' in boolean mode) DESC`
+          ),
+          ['book', 'ASC'],
+          ['chapter', 'ASC'],
+          ['verse', 'ASC']
+        ],
+        offset: page * 10,
+        limit: 10
+      });
+      return result;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+}
+
+const instance = new BibleService();
+export default instance;
---
+++ api/router/bible/index.ts
@@ -0,0 +1,58 @@
+import express = require('express');
+import LogQueue from '../../util/log-queue';
+import BibleService from './bibleService';
+
+const router = express.Router();
+
+//초기화 데이터 캐시
+const cache = {};
+
+/* 성경 리스트와 메타데이터를 가져온다. */
+router.get('/metadata', async (req, res, next) => {
+  try {
+    //캐시된 데이터가 있다면 사용
+    if (cache['metadata']) {
+      res.status(200).json(cache['metadata']);
+      return;
+    }
+    const metadata = await BibleService.getBibleMeta();
+    cache['metadata'] = metadata;
+    res.status(200).json(metadata);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* 메타데이터로 성경 검색 */
+router.get('/book/:book/chapter/:chapter/verse/:verse', async (req, res, next) => {
+  try {
+    const book = Number(req.params.book);
+    const chapter = Number(req.params.chapter);
+    const verse = Number(req.params.verse);
+    const page = Number(req.query.page) || 0;
+
+    const result = await BibleService.searchBibleByMeta(book, chapter, verse, page);
+
+    LogQueue.insertLog(req.useragent, { book: book, chapter: chapter, verse: verse, page: page });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* 키워드 검색, fulltext index */
+router.get('', async (req, res, next) => {
+  try {
+    const keyword = decodeURIComponent(req.query.keyword);
+    const page = Number(req.query.page);
+
+    const result = await BibleService.searchBibleByKeyword(keyword, page);
+
+    LogQueue.insertLog(req.useragent, { keyword: keyword, page: page });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
