9583ed1a4ae31692cc852727ae85499accdf8c27
Changehee Choi <changhee.dev@gmail.com>
2020-03-05 21:33:30 +0900

    Refactor review api with typescript

---
+++ api/router/review/review-service.ts
@@ -0,0 +1,34 @@
+import Review from '../../database/mongodb/models/review';
+import UserAgent from '../../models/user-agent';
+import { Details } from 'express-useragent';
+
+class ReviewService {
+  private static _instance: ReviewService;
+
+  private constructor() {
+    if (!ReviewService._instance) {
+      ReviewService._instance = this;
+    }
+    return ReviewService._instance;
+  }
+
+  async createReview(ua: Object, content: string) {
+    try {
+      const useragent = new UserAgent(ua);
+      const newReview = {
+        useragent: useragent.toObject(),
+        content: content
+      };
+      await Review.create(newReview);
+      console.log('Insert review...');
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  static getInstance() {
+    return new ReviewService();
+  }
+}
+
+export default ReviewService.getInstance();
