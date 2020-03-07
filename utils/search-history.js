d02ea24cf56920f50ca77e5388bdd886d466f02b
Changehee Choi <changhee.dev@gmail.com>
2020-03-07 22:36:42 +0900

    Change condition to check for duplicate history

--- utils/search-history.js
+++ utils/search-history.js
@@ -19,17 +19,16 @@ class SearchHistory {
       item => item.data.book === meta.book && item.data.chapter === meta.chapter && item.data.verse === meta.verse
     );
   }
-  findIndexByKeyword(from, text) {
-    return from.findIndex(item => item.data.text === text);
+  findIndexByKeyword(from, keywordData) {
+    return from.findIndex(item => item.data.keyword === keywordData.keyword && item.data.book === keywordData.book);
   }
   removeDuplHistory(histories, newHistory) {
     let index = 0;
     if (newHistory.type === 'keyword') {
-      index = this.findIndexByKeyword(histories, newHistory.data.text);
+      index = this.findIndexByKeyword(histories, newHistory.data);
     } else {
       index = this.findIndexByMeta(histories, newHistory.data);
     }
-
     //중복된 기록 삭제
     if (index !== -1) {
       histories.splice(index, 1);
@@ -50,37 +49,38 @@ class SearchHistory {
   }
   //이전 버전의 포맷으로 저장된 히스토리들을 업데이트 된 포맷으로 변경
   updatePrevVersionHistories() {
-    const histories = this.getSearchHistory();
-    if (histories) {
-      const newHistories = histories.reduce((ret, history) => {
-        if (!history.type) {
-          ret.push({
-            type: 'meta',
-            data: history
-          });
-        } else if (history.type === 'keyword' && !history.data.text) {
-          ret.push({
-            type: 'keyword',
-            data: {
-              text: history.data.keyword,
-              book: null,
-              keyword: history.data.keyword
-            }
-          });
-        } else {
-          ret.push(history);
-        }
+    const histories = this.getOrDefaultSearchHistory();
+    const newHistories = histories.reduce((ret, history) => {
+      if (!history.type) {
+        ret.push({
+          type: 'meta',
+          data: history
+        });
+      } else if (history.type === 'keyword' && !history.data.text) {
+        ret.push({
+          type: 'keyword',
+          data: {
+            text: history.data.keyword,
+            book: null,
+            keyword: history.data.keyword
+          }
+        });
+      } else {
+        ret.push(history);
+      }
 
-        return ret;
-      }, []);
-      this.updateLocalStorage(newHistories);
-    }
+      return ret;
+    }, []);
+    this.updateLocalStorage(newHistories);
   }
   clearSearchHistory() {
     localStorage.removeItem('searchHistory');
   }
+  static getInstance() {
+    return new SearchHistory();
+  }
 }
 
-const searchHistory = new SearchHistory();
+const searchHistory = SearchHistory.getInstance();
 
 export default searchHistory;
