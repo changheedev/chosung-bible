1a27f3bc7aa4ed1d0dc49ae18cba2de5caa9623e
Changhee Choi <changhee.dev@gmail.com>
2021-01-10 23:23:36 +0900

    feat: Health check API 추가 (#11)
    애플리케이션 동작 유무를 확인하기 위한 Health check API 를 추가함.

--- api/router/index.ts
+++ api/router/index.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.use('/bible', bible);
 router.use('/reviews', review);
+router.get('/health-check', (req, res, next) => {
+  res.send('OK');
+});
 
 export default router;
